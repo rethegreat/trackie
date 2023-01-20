3da2cdcd60f09bf2459b814ded3f31fad1e06b2b
Wo Ming Boaz Cheung <92407344+rethegreat@users.noreply.github.com>
2023-01-20 12:33:03 -0500

    created trackie login page

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -3,27 +3,38 @@ import "./LoginPage.css";
 
 function LoginPage() {
   return (
-    <div className="login-main">
-      <div className="left-login">
-        <h1>Sign In</h1>
+    <div>
+      <div className="logo">
+        <img src="/trackie-logo.jpg" className="trackie-logo" alt="" />
+        Trackie
+      </div>
+      <div className="login-main">
+        <div className="left-login">
+          <div>
+            <h1 className="title">Sign In</h1>
 
-        <div className="signup">
-          <div>New User?</div>
-          <div>Sign up here</div>
+            <div className="signup">
+              <div>New User?</div>
+              <div className="create-account">Create Account</div>
+            </div>
+          </div>
+
+          <input className="login-input" placeholder="Email" />
+
+          <input
+            className="login-input"
+            placeholder="Password"
+            type="password"
+          />
+          <div className="login-button">Sign In</div>
+
+          <div></div>
         </div>
 
-        <input placeholder="Email"/>
-        
-        <input placeholder="Password"/>
-        <div>Login</div>
-
-        <div></div>
+        <div className="right-login">
+          <img src="/login_shopping.jpg" className="shopping-image" alt="" />
+        </div>
       </div>
-
-    <div className="right-login">
-        <img src="/login_shopping.jpg" className="shopping-image" alt=""/>
-    </div>
-      
     </div>
   );
 }
